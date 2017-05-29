39ff575e8f39c5cf0358ef9ab3932189f482e465
minh.nguyen <minh.nguyen@tpssoft.com>
2017-05-29 14:20:24 +0700

    Project add package Microsoft.AspNetCore.Owin

---
+++ Driver.Api/Driver.Api/Middleware/DriverAspNetCoreMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Driver.Api.Middleware
+{
+    public class DriverAspNetCoreMiddleware
+    {
+        private readonly RequestDelegate Next;
+
+        public DriverAspNetCoreMiddleware(RequestDelegate next)
+        {
+            Next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            // do what ever you want using context.Request & context.Response
+            await Next.Invoke(context);
+        }
+    }
+}
