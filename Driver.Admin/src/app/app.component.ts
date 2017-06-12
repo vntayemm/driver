8e153aa686aa16051e7b14106d7918d632472b47
Minh Nguyen <phuminh88@gmail.com>
2017-06-13 01:01:12 +0700

    hero sample component

--- Driver.Admin/src/app/app.component.ts
+++ Driver.Admin/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  welcome = 'Welcome to driver license site';
 }
---
+++ Driver.Admin/src/app/hero/hero.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from 'app/hero/hero.model';
+
+@Component({
+  selector: 'hero-component',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+  }
+
+}
