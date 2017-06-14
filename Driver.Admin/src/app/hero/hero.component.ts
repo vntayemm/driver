922414f571acddf8e2a706ec952c267c17a402b0
Minh Nguyen <phuminh88@gmail.com>
2017-06-14 07:19:57 +0700

    add hero module

--- Driver.Admin/src/app/hero/hero.component.ts
+++ Driver.Admin/src/app/hero/hero.component.ts
@@ -8,10 +8,7 @@ import { Hero } from 'app/hero/hero.model';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  hero: Hero;
   heros: Hero[] = HEROES;
 
   constructor() { 
@@ -21,4 +18,20 @@ export class HeroComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.hero = hero;
+  }
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
