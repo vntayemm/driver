f0d448c794468fc725d3ddbd3a964102667355da
minh.nguyen <minh.nguyen@tpssoft.com>
2017-06-22 13:50:37 +0700

    delete

--- Driver.Admin/nativescript/src/app.ts
+++
@@ -1,7 +0,0 @@
-// nativescript
-import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
-
-// app
-import { NativeModule } from './native.module';
-
-platformNativeScriptDynamic().bootstrapModule(NativeModule);
