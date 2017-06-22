ff166a6b1ff01dada94270efe5aacc4bacf6860b
Minh Nguyen <phuminh88@gmail.com>
2017-06-23 03:09:59 +0700

    scss

---
+++ Driver.Admin/src/app/404/404.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-404-component',
+  templateUrl: './404.component.html',
+  styleUrls: ['./404.component.scss']
+})
+export class App404Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
