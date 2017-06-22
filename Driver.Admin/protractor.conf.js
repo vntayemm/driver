2b5473150c5aa2fdd8e294fbf0117472c1b45ecb
minh.nguyen <minh.nguyen@tpssoft.com>
2017-06-22 13:38:33 +0700

    add angular seed advance

--- Driver.Admin/protractor.conf.js
+++ Driver.Admin/protractor.conf.js
@@ -1,28 +1,56 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
+const config = {
+  baseUrl: 'http://localhost:5555/',
 
-const { SpecReporter } = require('jasmine-spec-reporter');
-
-exports.config = {
-  allScriptsTimeout: 11000,
   specs: [
-    './e2e/**/*.e2e-spec.ts'
+    './dist/e2e/**/*.e2e-spec.js'
   ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+
+  exclude: [],
+
+  // 'jasmine' by default will use the latest jasmine framework
   framework: 'jasmine',
+
+  // allScriptsTimeout: 110000,
+
   jasmineNodeOpts: {
+    // showTiming: true,
     showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
+    isVerbose: false,
+    includeStackTrace: false,
+    // defaultTimeoutInterval: 400000
   },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+
+  directConnect: true,
+
+  capabilities: {
+    browserName: 'chrome'
+  },
+
+  onPrepare: function() {
+    require("zone.js/dist/zone-node");
+    require("zone.js/dist/long-stack-trace-zone");
+    require("zone.js/dist/async-test");
+    require("zone.js/dist/fake-async-test");
+    require("zone.js/dist/sync-test");
+    require("zone.js/dist/proxy");
+    require("zone.js/dist/jasmine-patch");
+    browser.ignoreSynchronization = false;
+  },
+
+
+  /**
+   * Angular 2 configuration
+   *
+   * useAllAngular2AppRoots: tells Protractor to wait for any angular2 apps on the page instead of just the one matching
+   * `rootEl`
+   */
+  useAllAngular2AppRoots: true
 };
+
+if (process.env.TRAVIS) {
+  config.capabilities = {
+    browserName: 'firefox'
+  };
+}
+
+exports.config = config;
