6414b29dc8ccdeab905df084929e9b6cdb2151e9
Minh Nguyen <phuminh88@gmail.com>
2017-06-26 23:34:00 +0700

    delete webpack

---
+++ Driver.Admin/gulp/css.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var gulp = require('gulp');
+var path = require('path');
+var conf = require('./conf');
+
+gulp.task('css',function () {
+    
+});
--- Driver.Admin/src/app/hero/hero.component.ts
+++ Driver.Admin/src/app/hero/hero.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'hero-component',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
   private name: string = 'Nguyễn Phú Minh';
