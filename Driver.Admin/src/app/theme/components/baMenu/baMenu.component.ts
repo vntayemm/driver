ae92fb38edea6a6f565c3341dd3edb7f87e4852c
minh.nguyen <minh.nguyen@tpssoft.com>
2017-07-19 18:36:17 +0700

    type

--- Driver.Admin/src/app/theme/components/baMenu/baMenu.component.ts
+++ Driver.Admin/src/app/theme/components/baMenu/baMenu.component.ts
@@ -8,7 +8,7 @@ import { GlobalState } from '../../../global.state';
 @Component({
   selector: 'ba-menu',
   templateUrl: './baMenu.html',
-  styleUrls: ['./baMenu.scss']
+  styleUrls: ['./baMenu.scss'],
 })
 export class BaMenu {
 
@@ -17,30 +17,30 @@ export class BaMenu {
 
   @Output() expandMenu = new EventEmitter<any>();
 
-  public menuItems: any[];
+  menuItems: any[];
   protected _menuItemsSub: Subscription;
-  public showHoverElem: boolean;
-  public hoverElemHeight: number;
-  public hoverElemTop: number;
+  showHoverElem: boolean;
+  hoverElemHeight: number;
+  hoverElemTop: number;
   protected _onRouteChange: Subscription;
-  public outOfArea: number = -200;
+  outOfArea: number = -200;
 
   constructor(private _router: Router, private _service: BaMenuService, private _state: GlobalState) {
   }
 
-  public updateMenu(newMenuItems) {
+  updateMenu(newMenuItems) {
     this.menuItems = newMenuItems;
     this.selectMenuAndNotify();
   }
 
-  public selectMenuAndNotify(): void {
+  selectMenuAndNotify(): void {
     if (this.menuItems) {
       this.menuItems = this._service.selectMenuItem(this.menuItems);
       this._state.notifyDataChanged('menu.activeLink', this._service.getCurrentItem());
     }
   }
 
-  public ngOnInit(): void {
+  ngOnInit(): void {
     this._onRouteChange = this._router.events.subscribe((event) => {
 
       if (event instanceof NavigationEnd) {
@@ -56,20 +56,20 @@ export class BaMenu {
     this._menuItemsSub = this._service.menuItems.subscribe(this.updateMenu.bind(this));
   }
 
-  public ngOnDestroy(): void {
+  ngOnDestroy(): void {
     this._onRouteChange.unsubscribe();
     this._menuItemsSub.unsubscribe();
   }
 
-  public hoverItem($event): void {
+  hoverItem($event): void {
     this.showHoverElem = true;
     this.hoverElemHeight = $event.currentTarget.clientHeight;
     // TODO: get rid of magic 66 constant
     this.hoverElemTop = $event.currentTarget.getBoundingClientRect().top - 66;
   }
 
-  public toggleSubMenu($event): boolean {
-    let submenu = jQuery($event.currentTarget).next();
+  toggleSubMenu($event): boolean {
+    const submenu = jQuery($event.currentTarget).next();
 
     if (this.sidebarCollapsed) {
       this.expandMenu.emit(null);
